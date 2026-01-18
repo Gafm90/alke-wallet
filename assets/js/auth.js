97280aacd6b3ead6c9057caab14c3b66f0fb7ca5
= <gabriel.floresmendez90@gmail.com>
2026-01-18 19:44:40 -0300

    Limpieza de comentarios

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,21 +1,16 @@
-// Esperamos que cargue el formulario
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // evita que recargue la página
+  e.preventDefault(); 
 
-  // Credenciales simuladas
   const emailValido = "usuario@alke.cl";
   const passwordValida = "Talentodigital";
 
-  // Obtenemos valores del formulario
   const email = document.querySelector("input[type='email']").value;
   const password = document.querySelector("input[type='password']").value;
 
   if (email === emailValido && password === passwordValida) {
-    // Guardamos sesión
-    localStorage.setItem("logged", "true");
+       localStorage.setItem("logged", "true");
 
-    // Si no hay saldo, lo inicializamos
-    if (!localStorage.getItem("saldo")) {
+      if (!localStorage.getItem("saldo")) {
       localStorage.setItem("saldo", "100000");
     }
 
